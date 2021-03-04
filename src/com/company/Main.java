574d813929a59020ed6464be83337b9db8626f74
Dmitry Voloshko <79659195+voloshkodu@users.noreply.github.com>
2021-03-04 22:01:13 +0300

    L006 - Static/instance methods example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 
 import com.company.animals.Cat;
+import com.company.animals.Dog;
 
 public class Main {
 
@@ -16,5 +17,16 @@ public class Main {
 //        catL004.voice();
           Cat cat = new Cat("Barsik", 20);
           System.out.println(cat.jumpHeight());
+
+          Dog dog = new Dog();
+          dog.goToStick(14);
+          dog.whereTheDog();
+          dog.goToStick(20);
+          dog.whereTheDog();
+          dog.goToStick(5);
+          dog.whereTheDog();
+
+          Dog homelessDog = Dog.ofHomeless(100);
+
     }
 }
