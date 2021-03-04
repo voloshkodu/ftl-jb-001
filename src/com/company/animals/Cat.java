374d7a7bc63e97c88284432c306da72170ea7086
Dmitry Voloshko <79659195+voloshkodu@users.noreply.github.com>
2021-03-04 21:17:58 +0300

    L005 - methods examples

---
+++ src/com/company/animals/Cat.java
@@ -0,0 +1,20 @@
+package com.company.animals;
+
+public class Cat extends Animal {
+    public Cat(String name, Integer weight) {
+        super(name, weight);
+    }
+
+    public Cat() {
+    }
+
+    public Integer heightCoefficient(Integer weight){
+        if(weight > 10) return 120;
+        return 90;
+    }
+
+    public Integer jumpHeight() {
+        if (this.weight == null) return 300;
+        return this.heightCoefficient(this.weight);
+    }
+}
