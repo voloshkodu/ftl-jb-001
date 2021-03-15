333b512160d79aa8680f9473ea549ed841d03b10
Dmitry Voloshko <voloshkodu@gmail.com>
2021-03-15 16:12:16 +0300

    Task001 - renamed file/class Task002 - completed

--- src/com/company/tasks/task001/task001.java
+++ src/com/company/tasks/task001/task001.java
@@ -1,6 +1,6 @@
 package com.company.tasks.task001;
 
-public class task001 {
+public class Task001 {
     public static void run() {
         for (int i = 1; i < 6; i++) {
             System.out.print(i + " ");
